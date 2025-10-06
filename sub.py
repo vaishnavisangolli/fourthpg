9eb6ee5dd8f3085eb12fae18161602f695f3b9c7
vaishnavi sangolli <iam.vaishnavisangolli@gmail.com>
2025-10-06 12:09:08 +0530

    save to local

--- sub.py
+++ sub.py
@@ -10,3 +10,20 @@ if _name_ == "_main_":
 
     print("Addition:", add(x, y))
     print("Subtraction:", subtract(x, y))
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero is not allowed"
+
+if _name_ == "_main_":
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+
+    print("Addition:", add(x, y))
+    print("Subtraction:", subtract(x, y))
+    print("Multiplication:", multiply(x, y))
+    print("Division:", divide(x, y))
